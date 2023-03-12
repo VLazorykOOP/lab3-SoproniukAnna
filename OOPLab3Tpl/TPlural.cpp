87ae4b2bb03f8ccdb9a2144760b53ab0d457cb4e
SoproniukAnna <124136300+SoproniukAnna@users.noreply.github.com>
2023-03-12 13:37:05 +0200

    Клас TPlural, проектування

---
+++ OOPLab3Tpl/TPlural.cpp
@@ -0,0 +1,1 @@
+#include "TPlural.h"
---
+++ OOPLab3Tpl/TPlural.h
@@ -0,0 +1,34 @@
+#pragma once
+class TPlural
+{
+private:
+	unsigned int* arr = nullptr;
+	unsigned int beg;
+	unsigned int end;
+	unsigned int state; //0 ok; 1 memory error; 2 range read error; 3 range wrire error
+public:
+	TPlural();
+	TPlural(unsigned int);
+	TPlural(unsigned int, unsigned int);
+	TPlural(unsigned int, unsigned int, unsigned int);
+	TPlural(const TPlural&);
+	~TPlural();
+
+	void SetElement(unsigned int elem, unsigned int val);
+	unsigned int GetElement(unsigned int);
+	unsigned int GetState();
+
+	TPlural& operator = (const TPlural&);
+	
+	void Print();
+	void Union(const TPlural&);
+	void Crossing(const TPlural&);
+	void Difference(const TPlural&);
+
+	//по кількості елементів
+	bool More(); 
+	bool Less(); 
+	bool Equally();
+	bool NotEqually();
+};
+
